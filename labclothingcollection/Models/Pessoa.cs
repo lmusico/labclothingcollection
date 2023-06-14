f5dc85d5220bb8baa1397a675797ba2abfd9d256
Lucas Músico <lucasmusico23@hotmail.com>
2023-06-14 20:15:37 -0300

    Criação das models

---
+++ labclothingcollection/Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace labclothingcollection.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Nascimento { get; set; }
+
+        [Required]
+        public string Cpf { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+    }
+}
---
+++ labclothingcollection/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace labclothingcollection.Models
+{
+    public class Usuario : Pessoa
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Tipo { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+
+    }
+}
